88150459a9987c193c61f7bbcffced3478e7d8de
Stachu <bs.stasiak.bartosz@gmail.com>
2023-05-11 19:41:01 +0200

    Add working homing missles over net

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 var target = null
 const SPEED = 100.0
 
+@onready var explosion_scene = preload("res://scenes/bullet/rocket_explosion.tscn")
 
 func _ready():
 	self.velocity = -self.transform.basis.z * SPEED
@@ -20,7 +21,16 @@ func _physics_process(delta):
 	var collision = move_and_collide(self.velocity * delta)
 	
 	if collision:
-		self.call_deferred("queue_free")
+		self._explode()
+		
 
 func _on_timer_timeout():
+	self._explode()
+	
+
+func _explode():
+	var explosion: GPUParticles3D = explosion_scene.instantiate()
+	explosion.position = self.position
+	explosion.emitting = true
+	get_tree().current_scene.add_child(explosion)
 	self.call_deferred("queue_free")
